d8106e86fa0a886b2b6409336d124c90528b020d
[wkunzhi] <362416272@qq.com>
2019-06-01 21:02:56 +0800

    update translation import Meta class

--- decode_str/translation.py
+++ decode_str/translation.py
@@ -4,56 +4,89 @@
 
 import base64
 import zlib
-import json
-from datetime import datetime
 
 
-class Translation(object):
+class TranslationMetaClass(type):
+    """Meta 类"""
+    def __new__(mcs, name, bases, attrs):
+        count = 0
+        attrs['__decode__'] = []
+        for k, v in attrs.items():
+            if 'decode_' in k:
+                attrs['__decode__'].append(k)
+                count += 1
+        attrs['__TranslationFuncCount__'] = count
+        return type.__new__(mcs, name, bases, attrs)
 
-    @staticmethod
-    def load_base64(_key):
+
+class Decode(object):
+
+    def decode_base64(self):
         """解base64"""
-        return base64.b64decode(_key)
+        self._key = base64.b64decode(self._key)
 
-    @staticmethod
-    def load_zlib(_key):
-        """解压流"""
-        return zlib.decompress(_key)
+    def decode_zlib(self):
+        """解压串"""
+        self._key = zlib.decompress(self._key)
 
-    @staticmethod
-    def load_str(_key):
-        return str(_key, encoding="utf-8")
+    def decode_str(self):
+        """转字符串"""
+        return str(self._key, encoding="utf-8")
 
-    @staticmethod
-    def load_hex(_key):
+    def decode_hex(self):
         """转到16进制"""
-        return _key.hex()
+        self._key = self._key.hex()
+
+
+class Translation(Decode, metaclass=TranslationMetaClass, ):
+
+    def __init__(self, _key):
+        self._key = _key
+
+    def main(self):
+        choice = self.msg()
+        while choice != 'e':
+            if choice == '1':
+                self.decode_base64()
+            elif choice == '2':
+                self.decode_zlib()
+            elif choice == '3':
+                self.decode_hex()
+            self._print('blue', self._key)
+            choice = self.msg()
+
+        self._print('red', '调试结束')
+
+    def msg(self):
+        msg = """
+        1. base64 解码
+        2. 解压字符串
+        3. 转 16 进制
+        
+        b: 【返回上个选择】  e: 【退出】
+        """
+        self._print('yellow', msg)
+        return input('请选择').lower()
 
     @staticmethod
-    def go_16_10(_key):
-
-        return _key.hex(10)
-
-    @staticmethod
-    def hex_to_str(_key):
-        return ''.join([chr(i) for i in [int(b, 16) for b in _key.split(' ')]])
-
-    @staticmethod
-    def bin_to_str(_key):
-        return ''.join([chr(i) for i in [int(b, 2) for b in _key.split(' ')]])
+    def _print(color, msg):
+        """打印颜色控制
+        """
+        node = '\033[1;3{id}m{msg}\033[0m'
+        if color == 'yellow':
+            print(node.format(id=3, msg=msg))
+        elif color == 'red':
+            print(node.format(id=1, msg=msg))
+        elif color == 'green':
+            print(node.format(id=2, msg=msg))
+        elif color == 'blue':
+            print(node.format(id=4, msg=msg))
+        else:
+            print(node.format(id=7, msg=msg))
 
 
 if __name__ == '__main__':
-
     key = 'YyJr8BeAz5P07Lf/Dp2us2CYTYYLk4VmKCOrFfeAgB8u4wA/y9F+lT19vsPeFxnCBzH/myCrIeeJ52nzShYKmulf1+Fl6H6R2pg4NbBnIKMo9L1xcFcmF2e9vCPaJ8X70r2CtKXny8lP9KBVDkSz9w=='
-
-    ts = Translation()
-    a1 = ts.load_base64(key)
-    print(a1)
-    a2 = ts.load_hex(a1)
-    print(a2)
-
-
-    print(int('f7', 16))
-
-
+    _key = input('\033[1;31m输入解码内容>>> \033[0m')
+    ts = Translation(key)
+    ts.main()
