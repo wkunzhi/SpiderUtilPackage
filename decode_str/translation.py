b1193be78559bae22cebb37571172013c5fc755e
[wkunzhi] <362416272@qq.com>
2019-06-01 21:54:42 +0800

    add util object

--- decode_str/translation.py
+++ decode_str/translation.py
@@ -10,64 +10,18 @@ class TranslationMetaClass(type):
     """Meta 类"""
     def __new__(mcs, name, bases, attrs):
         count = 0
-        attrs['__decode__'] = []
+        attrs['__Decode__'] = []
         for k, v in attrs.items():
             if 'decode_' in k:
-                attrs['__decode__'].append(k)
                 count += 1
+                attrs['__Decode__'].append({count:k})
+        print(attrs['__Decode__'])
         attrs['__TranslationFuncCount__'] = count
         return type.__new__(mcs, name, bases, attrs)
 
 
-class Decode(object):
-
-    def decode_base64(self):
-        """解base64"""
-        self._key = base64.b64decode(self._key)
-
-    def decode_zlib(self):
-        """解压串"""
-        self._key = zlib.decompress(self._key)
-
-    def decode_str(self):
-        """转字符串"""
-        return str(self._key, encoding="utf-8")
-
-    def decode_hex(self):
-        """转到16进制"""
-        self._key = self._key.hex()
-
-
-class Translation(Decode, metaclass=TranslationMetaClass, ):
-
-    def __init__(self, _key):
-        self._key = _key
-
-    def main(self):
-        choice = self.msg()
-        while choice != 'e':
-            if choice == '1':
-                self.decode_base64()
-            elif choice == '2':
-                self.decode_zlib()
-            elif choice == '3':
-                self.decode_hex()
-            self._print('blue', self._key)
-            choice = self.msg()
-
-        self._print('red', '调试结束')
-
-    def msg(self):
-        msg = """
-        1. base64 解码
-        2. 解压字符串
-        3. 转 16 进制
-        
-        b: 【返回上个选择】  e: 【退出】
-        """
-        self._print('yellow', msg)
-        return input('请选择').lower()
-
+class Util(object):
+    """辅助类"""
     @staticmethod
     def _print(color, msg):
         """打印颜色控制
@@ -84,9 +38,54 @@ class Translation(Decode, metaclass=TranslationMetaClass, ):
         else:
             print(node.format(id=7, msg=msg))
 
+    def msg(self):
+        msg = """1. base64 解码\n2. 解压字符串\n3. 转 16 进制\n"""
+        self._print('yellow', msg)
+        print('b: 【返回】  e: 【退出】')
+        return input('>>>').lower()
+
+
+class Decode(Util, metaclass=TranslationMetaClass):
+    """
+    将需要添加的转码类型按下列类似格式添加即可
+    def decode_自定义名(self):
+        self._key = 解密过程
+    """
+    def __init__(self, _key):
+        self._key = _key
+
+    def main(self):
+        choice = self.msg()
+        while choice != 'e':
+            if choice == '1':
+                self.decode_base64()
+            elif choice == '2':
+                self.decode_zlib()
+            elif choice == '3':
+                self.decode_hex()
+            self._print('blue', self._key)
+            choice = self.msg()
+        self._print('red', '调试结束')
+
+    def decode_base64(self):
+        """解base64"""
+        self._key = base64.b64decode(self._key)
+
+    def decode_zlib(self):
+        """解压串"""
+        self._key = zlib.decompress(self._key)
+
+    def decode_str(self):
+        """转字符串"""
+        self._key = str(self._key, encoding="utf-8")
+
+    def decode_hex(self):
+        """转到16进制"""
+        self._key = self._key.hex()
+
 
 if __name__ == '__main__':
-    key = 'YyJr8BeAz5P07Lf/Dp2us2CYTYYLk4VmKCOrFfeAgB8u4wA/y9F+lT19vsPeFxnCBzH/myCrIeeJ52nzShYKmulf1+Fl6H6R2pg4NbBnIKMo9L1xcFcmF2e9vCPaJ8X70r2CtKXny8lP9KBVDkSz9w=='
-    _key = input('\033[1;31m输入解码内容>>> \033[0m')
-    ts = Translation(key)
+    _key = 'YyJr8BeAz5P07Lf/Dp2us2CYTYYLk4VmKCOrFfeAgB8u4wA/y9F+lT19vsPeFxnCBzH/myCrIeeJ52nzShYKmulf1+Fl6H6R2pg4NbBnIKMo9L1xcFcmF2e9vCPaJ8X70r2CtKXny8lP9KBVDkSz9w=='
+    # _key = input('\033[1;31m输入解码内容>>> \033[0m')
+    ts = Decode(_key)
     ts.main()
